93b84bce36f54ac7ed987d92ffadd49aa0ad3525
Luis Gomez <lgomez00714@gmail.com>
2025-08-21 14:10:45 -0700

    Added new components for search, filters, and cards; updated profile and index screens to utilize these components; included use-debounce library for search functionality; updated package dependencies.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,19 +1,80 @@
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Search from "@/components/Search";
+import { FeaturedCard, Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import { useGlobalContext } from "@/lib/global-provider";
 
 export default function Index() {
+  const { user } = useGlobalContext();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/properties/1">Property 1</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={[1, 2]}
+        renderItem={(item) => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex-flex-row">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className="size-12 rounded-full"
+                />
+                <View className="flex flex-col items-start justify-center ml-2">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium">
+                    {user?.name}
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+            <Search />
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                data={[1, 2, 3, 4]}
+                renderItem={(item) => <FeaturedCard />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                bounces={false}
+                showsHorizontalScrollIndicator={false}
+                contentContainerClassName="gap-5 flex mt-5"
+              />
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Our Recommendations
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Filters />
+          </View>
+        }
+      />
+    </SafeAreaView>
   );
 }
